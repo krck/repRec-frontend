08e6ffde69f7ca5b11ab6244e6948e1384a23c23
Peter Baumann <peterbaumann2@gmx.de>
2024-12-19 17:28:51 +0100

    v0.0.1 - Basics are working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    version: '0.0.0',
+    version: '0.0.1',
     apiUrl: 'https://www.reprec.de',
     auth: {
         domain: 'dev-reprec.eu.auth0.com',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    version: '0.0.0',
+    version: '0.0.1',
     apiUrl: 'http://localhost:5205',
     auth: {
         domain: 'dev-reprec.eu.auth0.com',
