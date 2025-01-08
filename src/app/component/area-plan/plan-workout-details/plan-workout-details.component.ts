a4586e488ee32a441e63a3c9ec321b3e50ec4e23
Peter Baumann <peterbaumann2@gmx.de>
2025-01-08 21:49:51 +0100

    PlanWorkout Exercise drag and drop implemented

--- src/app/component/area-plan/plan-workout-details/plan-workout-details.component.ts
+++ src/app/component/area-plan/plan-workout-details/plan-workout-details.component.ts
@@ -1,3 +1,4 @@
+import { DragDropModule, CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList, } from '@angular/cdk/drag-drop';
 import { ShareServicePlanWorkout } from '../../../service/share-service-planWorkout';
 import { MatButtonModule } from '@angular/material/button';
 import { PlanWorkout } from '../../../models/planWorkout';
@@ -9,13 +10,22 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-plan-workout-details',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, DragDropModule, CdkDropList, CdkDrag],
   templateUrl: './plan-workout-details.component.html',
   styleUrl: './plan-workout-details.component.scss'
 })
 export class PlanWorkoutDetailsComponent implements OnInit {
   planWorkoutId!: number;
   planWorkout!: PlanWorkout;
+
+
+  mondayArr: string[] = ['Dead lift', 'Bench Press', 'Squat', 'Pull-up', 'Dumbbell Curl'];
+  tuesdayArr: string[] = ['Hammer Curl', 'Tricep Extension', 'Shoulder Press', 'Leg Press', 'Leg Curl'];
+  wednesdayArr: string[] = [];
+  thursdayArr: string[] = [];
+  fridayArr: string[] = [];
+  saturdayArr: string[] = [];
+  sundayArr: string[] = [];
 
   constructor(private route: ActivatedRoute, private router: Router, private shareServicePlanWorkout: ShareServicePlanWorkout) { }
 
@@ -32,5 +42,20 @@ export class PlanWorkoutDetailsComponent implements OnInit {
   openDialog() {
     // Open dialog to edit the selected plan workout
   }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+
 
 }
