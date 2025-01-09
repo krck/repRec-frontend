7cc81e610347d10377d6a50a3bed061714600ef7
Peter Baumann <peterbaumann2@gmx.de>
2025-01-09 21:59:37 +0100

    PlanWorkout Exercise logic #2

--- src/app/component/area-plan/plan-workout-details/plan-workout-details.component.ts
+++ src/app/component/area-plan/plan-workout-details/plan-workout-details.component.ts
@@ -2,6 +2,7 @@ import { PlanworkoutexerciseDialogComponent } from '../../dialogs/planworkoutexe
 import { DeleteConfirmationDialogComponent } from '../../dialogs/delete-confirmation-dialog/delete-confirmation-dialog.component';
 import { DragDropModule, CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList, } from '@angular/cdk/drag-drop';
 import { ShareServicePlanWorkout } from '../../../service/share-service-planWorkout';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { PlanWorkoutExercise } from '../../../models/planWorkoutExercise';
 import { MatButtonModule } from '@angular/material/button';
 import { PlanWorkout } from '../../../models/planWorkout';
@@ -16,7 +17,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-plan-workout-details',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule, DragDropModule, MatMenuModule, CdkDropList, CdkDrag],
+  imports: [CommonModule, MatIconModule, MatButtonModule, DragDropModule, MatMenuModule, MatProgressSpinnerModule, CdkDropList, CdkDrag],
   templateUrl: './plan-workout-details.component.html',
   styleUrl: './plan-workout-details.component.scss'
 })
@@ -24,6 +25,8 @@ export class PlanWorkoutDetailsComponent implements OnInit {
 
   planWorkoutId!: number;
   planWorkout!: PlanWorkout;
+
+  loading: boolean = true;
 
   mondayArr: PlanWorkoutExercise[] = [];
   tuesdayArr: PlanWorkoutExercise[] = [];
@@ -46,6 +49,27 @@ export class PlanWorkoutDetailsComponent implements OnInit {
     this.planWorkoutId = Number.parseInt(this.route.snapshot.paramMap.get('id')!);
   }
 
+  fetchData(): void {
+    this.loading = true;
+    this.apiService.getPlanWorkoutExercises(this.planWorkoutId).subscribe(
+      (response) => {
+        // Split the exercises into the days of the week by "dayIndex"
+        this.mondayArr = response.filter((e) => e.dayIndex === 0).sort((a, b) => a.dayOrder - b.dayOrder);
+        this.tuesdayArr = response.filter((e) => e.dayIndex === 1).sort((a, b) => a.dayOrder - b.dayOrder);
+        this.wednesdayArr = response.filter((e) => e.dayIndex === 2).sort((a, b) => a.dayOrder - b.dayOrder);
+        this.thursdayArr = response.filter((e) => e.dayIndex === 3).sort((a, b) => a.dayOrder - b.dayOrder);
+        this.fridayArr = response.filter((e) => e.dayIndex === 4).sort((a, b) => a.dayOrder - b.dayOrder);
+        this.saturdayArr = response.filter((e) => e.dayIndex === 5).sort((a, b) => a.dayOrder - b.dayOrder);
+        this.sundayArr = response.filter((e) => e.dayIndex === 6).sort((a, b) => a.dayOrder - b.dayOrder);
+        this.loading = false;
+      },
+      (error) => {
+        this.loading = false;
+      }
+    );
+  }
+
+
   // Navigate back to the main admin options list
   goBack() {
     this.router.navigate(['/plan-workout']);
@@ -59,7 +83,7 @@ export class PlanWorkoutDetailsComponent implements OnInit {
           // Overwrite with the updated (saved to DB) values
           Object.assign(planWorkoutExercise, result);
         } else {
-          // Add the new planWorkout to the list (UI refresh only by recreating the whole array)
+          // Add the new planWorkoutExercise to the list (UI refresh only by recreating the whole array)
           //this.planWorkoutRecords = [...this.planWorkoutRecords, result].sort((a, b) => { return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(); });
           //this.planWorkoutDataSource = new MatTableDataSource<PlanWorkout>(this.planWorkoutRecords);
         }
@@ -75,7 +99,7 @@ export class PlanWorkoutDetailsComponent implements OnInit {
     const dialogRef = this.dialog.open(DeleteConfirmationDialogComponent);
     dialogRef.afterClosed().subscribe((confirmed) => {
       if (confirmed === true) {
-        // Delete the planWorkout and remove from the list 
+        // Delete the planWorkoutExercise and remove from the list 
         this.apiService.deletePlanWorkout(planWorkoutExercise.id).subscribe(
           (response) => {
             //this.planWorkoutRecords = this.planWorkoutRecords.filter((c) => c.id !== planWorkoutId);
